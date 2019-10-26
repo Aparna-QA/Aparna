bf69f4b96df6e98e203f70381c17c1807e5214ad
Vinod Savanapelly <Vinod Savanapelly@192.168.1.16>
2019-10-26 11:31:08 +0530

    My First Commit

---
+++ src/test/java/com/examples/test1.java
@@ -0,0 +1,33 @@
+package com.examples;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class test1 {
+	
+	@Test(priority=3)
+	public void add()
+	{
+		System.out.println("addition");
+	}
+	
+	@Test(priority=1)
+	public void sub()
+	{
+		System.out.println("subtraction");
+	}
+
+	@Test(priority=2)
+	public void mul()
+	{
+		System.out.println("multiplication");
+	}
+	
+	@Test(priority=4)
+	public void login()
+	{
+		FirefoxDriver driver=new FirefoxDriver();
+		driver.get("https://www.google.com/");
+	}
+	
+}
